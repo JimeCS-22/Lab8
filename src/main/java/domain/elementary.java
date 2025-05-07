4cf4ac83ab2c9db1233339b439b62c7eb7799596
Jimena Calvo <jimenacalvosoto13@gmail.com>
2025-05-07 17:19:13 -0600

    Clase

--- src/main/java/domain/elementary.java
+++ src/main/java/domain/elementary.java
@@ -2,108 +2,63 @@ package domain;
 
 public class elementary {
 
-    //Algoritmo de burbuja
-    public void BubleSort(int [] a){
-
-        for (int i = 1; i < a.length-i; i++){
-
-            for (int j = 0; j<a.length-i; j++){
-
-                if (a[j] > a [j+1]){
-
-                    int aux = a[j];
-                    a[j]= a[j+1];
-                    a[j+1]= aux ;
-
-                }//if
-            }//for
-        }//for
-
-    }
-
-    //Algoritmo burbuja mejorada
-    public void improvebubleSort(int[] a){
-
-        boolean swapped = true; //Intercambios
-
-        for(int i = 1 ; swapped; i++){
-
-            swapped = false ;
-
-            for(int j = 0; j<a.length-i ; j++){
-
-                if (a[j] > a[j+1]){
-
-                    int aux = a[j];
-
-                    a[j] = a[j+1];
-
-                    a[j+1] = aux ;
-
-                    swapped = true ;
-                }//if
-
-            }//for
-
-        }//for
-
-    }
-
-    //Algoritmo de seleccion
-    public void selectionSor(int[] a){
-
-        for (int i = 0; i<a.length-1; i++){
-
-            int min = a[i];
-            int minIndex = i ;
-
-            for (int j = i+1; j<a.length; j++){
-
-                if (a[j]<min){
-
-                    min = a[j];
-                    minIndex = j ;
-
+    public void bubbleSort(int a[]){
+        for(int i=1;i<a.length;i++)
+            for(int j=0;j<a.length-i;j++){
+                if(a[j]>a[j+1]){
+                    int aux=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=aux;
                 }//if
             }//for j
-
-            a[minIndex] = a[i];
-            a[i] = min ;
-        }//For
-
     }
 
-    //Algoritmo de seleccion con contador
-    public void countingSort(int [] a){
+    public void improvedBubbleSort(int a[]){
+        boolean swapped = true; //intercambiado
+        for(int i=1;swapped;i++){
+            swapped = false;
+            for(int j=0;j<a.length-i;j++){
+                if(a[j]>a[j+1]){
+                    int aux=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=aux;
+                    swapped = true;
+                }//if
+            }//for j
+        }//for i
+    }
 
-        int max = maxArray(a);
-        int[] counter = new int [max + 1];
+    public void selectionSort(int a[]){
+        for(int i=0;i<a.length-1;i++){
+            int min=a[i];
+            int minIndex=i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]<min){
+                    min=a[j];
+                    minIndex=j;
+                }//if
+            }//for j
+            a[minIndex]=a[i];
+            a[i]=min;
+        }//for i
+    }
 
-        for (int element : a ) {
-
-            counter[element]++;
-
-        }//for
-
+    public void countingSort(int a[]) {
+        int max = util.Utility.maxArray(a); //va de 0 hasta el elemento maximo
+        // create buckets
+        int counter[] = new int[max + 1];
+        // fill buckets
+        for (int element : a) {
+            counter[element]++; //incrementa el num de ocurrencias del elemento
+        }
+        // sort array
         int index = 0;
-
-        for (int i = 0; i<counter.length; i++){
-
-            while(counter[i]>0){
-
-                a[index ++] = i ;
-                counter[i] --;
-
-            }//while
-
-        }//for
-
+        for (int i = 0; i < counter.length; i++) {
+            while (counter[i]>0) { //significa q al menos hay un elemento (q existe)
+                a[index++] = i;
+                counter[i]--;
+            }
+        }//for i
     }
-
-    //Algoritmo para encontrar el maximo
-    private int maxArray(int[] a) {
-        return 0;
-    }
-
 
 }
--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -172,6 +172,7 @@ public class Utility {
         }
         return mayor;
     }
+
 }
 
     /*
