327eb4eeca930f536cc3bdc4d4d5e196f4d1fbf4
Jimena Calvo <jimenacalvosoto13@gmail.com>
2025-05-07 17:36:20 -0600

    Cambios

---
+++ src/test/java/domain/elementaryTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class elementaryTest {
+
+    @Test
+    void elementaryTest(){
+
+        /*Parámetros:
+        String algorithm: es el nombre del algoritmo a utilizar
+        int[] array: representa el arreglo inicial no ordenado
+        int n: representa la cantidad de elementos a mostrar por consola
+
+         */
+
+        
+
+
+
+
+
+
+    }
+
+}
