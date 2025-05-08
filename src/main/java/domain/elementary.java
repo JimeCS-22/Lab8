ba268b1a3733172105ddd8d31d612c7a6ec98823
Zailyn Tencio <zailynten@gmail.com>
2025-05-07 18:04:43 -0600

    Creación de elementarySorting

--- src/main/java/domain/elementary.java
+++ src/main/java/domain/elementary.java
@@ -1,6 +1,24 @@
 package domain;
 
 public class elementary {
+    private static int totalIteractions;
+    private static int totalChanges;
+
+    public static int getTotalIteractions() {
+        return totalIteractions;
+    }
+
+    public static void setTotalIteractions(int totalIteractions) {
+        elementary.totalIteractions = totalIteractions;
+    }
+
+    public static int getTotalChanges() {
+        return totalChanges;
+    }
+
+    public static void setTotalChanges(int totalChanges) {
+        elementary.totalChanges = totalChanges;
+    }
 
     private static int totalIteraciones;
     private static int TotalCount;
@@ -22,6 +40,8 @@ public class elementary {
     }
 
     public static void bubbleSort(int a[]){
+        totalIteractions = 0;
+        totalChanges = 0;
         for(int i=1;i<a.length;i++)
             for(int j=0;j<a.length-i;j++){
                 if(a[j]>a[j+1]){
@@ -33,6 +53,8 @@ public class elementary {
     }
 
     public static void improvedBubbleSort(int a[]){
+        totalIteractions = 0;
+        totalChanges = 0;
         boolean swapped = true; //intercambiado
         for(int i=1;swapped;i++){
             swapped = false;
@@ -48,6 +70,8 @@ public class elementary {
     }
 
     public static void selectionSort(int a[]){
+        totalIteractions = 0;
+        totalChanges = 0;
         for(int i=0;i<a.length-1;i++){
             int min=a[i];
             int minIndex=i;
--- src/test/java/domain/elementaryTest.java
+++ src/test/java/domain/elementaryTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import static domain.elementary.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class elementaryTest {
@@ -9,14 +10,28 @@ class elementaryTest {
     @Test
     void elementaryTest(){
 
+    }
 
+    public String elementarySorting(String algorithm, int[] a, int n) {
+        switch (algorithm.toLowerCase()) {
+            case "bubblesort":
+                bubbleSort(a);
+                break;
+            case "improvedBubbleSort":
+                improvedBubbleSort(a);
+                break;
+            case "selectionSort":
+                selectionSort(a);
+                break;
+            case "countingSort":
+                countingSort(a);
+                break;
+            default:
+                return "Algoritmo no reconocido. Use BubbleSort, ImprovedBubbleSort, SelectionSort o CountingSort.";
+        }
 
-
-
-
-
-
-
+        String result = "";
+        return result;
     }
 
 }
