bd376072f240d5194bdbe27a9671a4f773782a1f
Zailyn Tencio <zailynten@gmail.com>
2025-05-07 18:17:55 -0600

    Creación de elementaryTest

--- src/test/java/domain/elementaryTest.java
+++ src/test/java/domain/elementaryTest.java
@@ -8,10 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class elementaryTest {
 
     @Test
-    void elementaryTest(){
+    void elementarytest(){
+        int[] a = util.Utility.getIntegerArray(10000);
+        int[] b = util.Utility.copyArray(a); //no utilice Arrays.copyOf(a, a.length)
+        System.out.println(elementarySorting("bubblesort", a, 50));
+        System.out.println(elementarySorting("improvedBubbleSort", b, 100));
+        System.out.println(elementarySorting("selectionSort", util.Utility.getIntegerArray(10000), 150));
+        System.out.println(elementarySorting("countingSort", util.Utility.getIntegerArray(10000), 200));
     }
 
-    public String elementarySorting(String algorithm, int[] a, int n) {
+    public static String elementarySorting(String algorithm, int[] a, int n) {
         switch (algorithm.toLowerCase()) {
             case "bubblesort":
                 bubbleSort(a);
